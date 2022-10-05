02567e2f6ab740bb0e37e731345ec6c73584bae0
Mrhetsko <mrhetsko@gmail.com>
2022-10-05 09:03:37 +0200

    05.10

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 # endpoint = 'https://httpbin.org/status/200/'
 endpoint = 'https://httpbin.org/anything'
 
-responce = requests.get(endpoint, json={"query": "Hello world"})
+responce = requests.get(endpoint)
 # print(responce.text)
 # print(responce.json())
 print(responce.status_code)
